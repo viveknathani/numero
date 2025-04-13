67760c9d6fa335635f84a73c0a3a7a72e6fbaafe
viveknathani <viveknathani2402@gmail.com>
2025-04-13 16:47:47 +0530

    feat: create nlog

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/viveknathani/numero/nlog"
+
+func main() {
+	nlog.Info("hello from numero!")
+}
---
+++ nlog/nlog.go
@@ -0,0 +1,34 @@
+package nlog
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+// Info should be used for informational logging.
+func Info(args ...any) {
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel == "INFO" || logLevel == "DEBUG" {
+		logInternal("[INFO]", args...)
+	}
+}
+
+// Debug should be used for debugging level logging.
+func Debug(args ...any) {
+	if os.Getenv("LOG_LEVEL") == "DEBUG" {
+		logInternal("[DEBUG]", args...)
+	}
+}
+
+// Error should be used for error logging. Will be emitted always.
+func Error(args ...any) {
+	logInternal("[ERROR]", args...)
+}
+
+// logInternal is the internal implementation of numero's lightweight logger
+func logInternal(level string, args ...any) {
+	currentTime := time.Now()
+	prefix := []any{level, currentTime.Format(time.DateTime)}
+	fmt.Println(append(prefix, args...)...)
+}
