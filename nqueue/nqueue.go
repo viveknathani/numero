619302aa25874d3061a501c6d1534036a41f70ed
viveknathani <viveknathani2402@gmail.com>
2025-05-08 18:16:25 +0530

    feat: nqueue

---
+++ nqueue/nqueue.go
@@ -0,0 +1,42 @@
+package nqueue
+
+// NQueue is a simple queue
+type NQueue[T any] struct {
+	elements []T
+}
+
+// Enqueue adds an element to the end of the queue
+func (nqueue *NQueue[T]) Enqueue(element T) {
+	nqueue.elements = append(nqueue.elements, element)
+}
+
+// Dequeue removes an element from the front of the queue
+func (nqueue *NQueue[T]) Dequeue() (T, bool) {
+	if len(nqueue.elements) == 0 {
+		var zero T
+		return zero, false
+	}
+	element := nqueue.elements[0]
+	nqueue.elements = nqueue.elements[1:]
+	return element, true
+}
+
+// Peek returns an element from the front of the queue
+func (nqueue *NQueue[T]) Peek() (T, bool) {
+	if len(nqueue.elements) == 0 {
+		var zero T
+		return zero, false
+	}
+	element := nqueue.elements[0]
+	return element, true
+}
+
+// IsEmpty checks if queue is empty
+func (nqueue *NQueue[T]) IsEmpty() bool {
+	return len(nqueue.elements) == 0
+}
+
+// Len returns length of the queue
+func (nqueue *NQueue[T]) Len() int {
+	return len(nqueue.elements)
+}
